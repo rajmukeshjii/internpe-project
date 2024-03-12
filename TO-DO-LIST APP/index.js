4743290abb87c5f4a7f5b2ed74759579e739e99d
MUKESH RAJ <rajmukeshjii2000@gmail.com>
2024-03-12 10:53:03 +0530

    initial

---
+++ TO-DO-LIST APP/index.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const inputBox = document.getElementById("input-box");
+    const listContainer = document.getElementById("list-container");
+
+    addBtn.addEventListener("click",addTask);
+    
+    function addTask(){
+        if (inputBox.value === '') {
+            alert("You must write something!");
+        } 
+        else {
+            let li = document.createElement("li");
+            li.innerHTML = inputBox.value;
+            listContainer.appendChild(li);
+            let span = document.createElement("span");
+            span.innerHTML = "\u00d7";
+            li.appendChild(span);
+            inputBox.value = "";
+            saveData();
+        }
+    }
+
+    listContainer.addEventListener("click", function(e) {
+        if (e.target.tagName === "LI") {
+            e.target.classList.toggle("checked");
+            saveData();
+        } else if (e.target.tagName === "SPAN") {
+            e.target.parentElement.remove();
+            saveData();
+        }
+    });
+
+    function saveData() {
+        localStorage.setItem("tasks", listContainer.innerHTML);
+    }
+
+    function showTasks() {
+        listContainer.innerHTML = localStorage.getItem("tasks") || '';
+    }
+    showTasks();
+});
